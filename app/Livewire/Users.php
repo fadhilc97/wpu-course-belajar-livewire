01b66620a258fa7a7cab998234f727e4630a7296
Fadhil <fadhil@incit.org>
2025-11-03 21:49:16 +0700

    Initial commit

---
+++ app/Livewire/Users.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\User;
+use Livewire\Component;
+use Illuminate\Support\Facades\Hash;
+
+class Users extends Component
+{
+  public $name = '';
+  public $email = '';
+  public $password = '';
+
+  public function createNewUser() {
+    User::create([
+      'name' => $this->name,
+      'email' => $this->email,
+      'password' => Hash::make($this->password)
+    ]);
+
+    $this->reset();
+  }
+
+  public function render()
+  {
+    return view('livewire.users', [
+      'title' => 'Users Page',
+      'users' => User::all()
+    ]);
+  }
+}
---
+++ resources/views/livewire/users.blade.php
@@ -0,0 +1,55 @@
+<div class="w-1/2 my-5 mx-auto">
+  <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Create New User</h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form wire:submit.prevent="createNewUser" action="#" method="POST" class="space-y-6">
+        <div>
+          <label for="name" class="block text-sm/6 font-medium text-gray-900">Name</label>
+          <div class="mt-2">
+            <input wire:model="name" id="name" type="text" name="name" required autocomplete="name"
+              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+          </div>
+        </div>
+
+        <div>
+          <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+          <div class="mt-2">
+            <input wire:model="email" id="email" type="email" name="email" required autocomplete="email"
+              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+          </div>
+        </div>
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
+          </div>
+          <div class="mt-2">
+            <input wire:model="password" id="password" type="password" name="password" required
+              autocomplete="current-password"
+              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+          </div>
+        </div>
+
+        <div>
+          <button type="submit"
+            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
+            in</button>
+        </div>
+      </form>
+    </div>
+  </div>
+
+
+
+  <hr class="border my-5" />
+
+  <h2 class="text-2xl font-semibold">Users List</h2>
+  <ul class="list-disc">
+    @foreach ($users as $user)
+      <li>{{ $user->name }}</li>
+    @endforeach
+  </ul>
+</div>
